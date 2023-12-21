8c24102cdfd6cd842d0f616fadf28c44dd37c489
Michal Pawlik <michal.pawlik@fenergo.com>
2023-12-21 11:00:03 +0100

    fix build

---
+++ tests/TestUtils/Collections/MongoDbFixtureCollection.cs
@@ -0,0 +1,7 @@
+﻿using TradingApp.TestUtils.Fixtures;
+using Xunit;
+
+namespace TradingApp.TestUtils.Collections;
+
+[CollectionDefinition(nameof(MongoDbFixtureCollection))]
+public class MongoDbFixtureCollection : ICollectionFixture<MongoDbFixture> { }
